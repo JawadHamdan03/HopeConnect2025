20cc118032140794c59b2d795824ce1a09a7bb07
Mohammad Murad Esawi <146820485+mohammadEsawi@users.noreply.github.com>
2025-05-06 00:45:00 +0300

    Update server.js
    fix conflict

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import express from 'express';
 import dotenv from 'dotenv';
 import orphanRoutes from './routes/orphanRoutes.js';
@@ -6,16 +5,6 @@ import userRoutes from './routes/userRoutes.js';
 import donationRoutes from './routes/donationRoutes.js'; 
 
 dotenv.config();
-=======
-import express from "express";
-import orphans from "./routes/orphanRoutes.js";
-import users from "./routes/userRoutes.js";
-import donation from "./routes/donationRoutes.js";
-import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './utils/swagger.js';
-import orphanageRoutes from "./routes/orphanageRoutes.js"
-
->>>>>>> Stashed changes
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -35,20 +24,5 @@ app.use('/api/users', userRoutes);
 <<<<<<< Updated upstream
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-=======
-app.use("/api/users", users);
 
-app.use("/api/donations",donation);
-
-app.use('/api/orphanages', orphanageRoutes);
-
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
->>>>>>> Stashed changes
 });
